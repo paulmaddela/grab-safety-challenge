ccb7ab00f700adeaa4dcdef87f01d973411b2ee0
paulmaddela <pauldinakar@gmail.com>
2019-06-18 10:24:17 +0530

    Update prediction.R

--- prediction.R
+++ prediction.R
@@ -2,6 +2,9 @@ library(caret)
 
 preprocessed_test_data <- readRDS("output/preprocessed_test_data")
 
+required_columns <- names(preprocessed_test_data)[ - grep("_combn_",names(preprocessed_test_data))]
+preprocessed_test_data <- preprocessed_test_data[,required_columns]
+
 rf_model <- readRDS("output/rf_model")
 
 predictions <- as.data.frame(predict(rf_model,preprocessed_test_data, type = "prob"))
@@ -10,5 +13,8 @@ head(predictions)
 predictions$predict <- names(predictions)[1:2][apply(predictions[,1:2], 1, which.max)]
 
 # Replace ______ with the  true values of predictions.
+predictions$predict <- as.factor(predictions$predict)
 predictions$observed <- _______________________
+confusionMatrix(predictions$predict,predictions$observed)
+plot(roc(as.numeric(predictions$predict),as.numeric(predictions$observed)))
 plot(roc(predictions))
